72304e11d25012ffe7c3f7b536367d9b92880ddf
aleksandar <alpe88.dev@gmail.com>
2020-04-27 14:32:15 -0700

    gitignore and skeleton for #2

---
+++ javascript/two-fer/two-fer.js
@@ -0,0 +1,7 @@
+//
+// This function is meant to print `One for ${personName}, one for me.` as a solution to excercise .
+//
+
+export const twoFer = (personName) => {
+  return `One for ${personName}, one for me.`;
+};
