d61808468f61b0f70e5b47defe0e98faf0f5fe01
aleksandar <alpe88.dev@gmail.com>
2020-04-27 19:46:48 -0700

    added param with default

--- javascript/two-fer/two-fer.js
+++ javascript/two-fer/two-fer.js
@@ -2,6 +2,6 @@
 // This function is meant to print `One for ${personName}, one for me.` as a solution to excercise .
 //
 
-export const twoFer = (personName) => {
+export const twoFer = (personName = `you`) => {
   return `One for ${personName}, one for me.`;
 };
